5fa75b38fab6b4c19ced970405345e6aa46b21d5
Jessicalong627 <jessicalong627@gmail.com>
2019-01-07 16:58:55 -0500

    added search feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts';
 
 class App extends Component {
+
+  state = {
+    contacts: [ {
+        "id": "karen",
+        "name": "Karen Isgrigg",
+        "handle": "karen_isgrigg",
+        "avatarURL": "http://localhost:5001/karen.jpg"
+      },
+      {
+        "id": "richard",
+        "name": "Richard Kalehoff",
+        "handle": "richardkalehoff",
+        "avatarURL": "http://localhost:5001/richard.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tylermcginnis",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  }
+
+  removeContact = (contact) => {
+    this.setState((current) => ({
+        contacts: current.contacts.filter((c) => {
+          return c.id !== contact.id
+        })
+    }))
+  }
+
   render() {
     return (
       <div>
-        initial commit
+        <ListContacts
+          onDeleteContact={this.removeContact}
+          contacts={this.state.contacts} />
       </div>
     );
   }
